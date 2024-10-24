b766d83c561097c6d767d380e9cd5a77fe1d33c3
marindeniaud <pkap009@live.rhul.ac.uk>
2024-10-24 16:57:33 +0100

    Add a new class for ideal gas calculation
    Former-commit-id: 3a10b56ad9922f1857e53e56687ec7628a2c8c06

---
+++ include/BDSIdealGas.hh
@@ -0,0 +1,33 @@
+/* 
+Beam Delivery Simulation (BDSIM) Copyright (C) Royal Holloway, 
+University of London 2001 - 2024.
+
+This file is part of BDSIM.
+
+BDSIM is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published 
+by the Free Software Foundation version 3 of the License.
+
+BDSIM is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BDSIM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef BDSIDEALGAS_H
+#define BDSIDEALGAS_H
+
+#include "globals.hh" // geant4 globals / types
+#include <list>
+
+class BDSIdealGas{
+public:
+    G4double CalculateDensityFromPressureTemperature(const std::list<G4String>& components,
+                                                     const std::list<Type>&     componentFractions,
+                                                     G4double pressure,
+                                                     G4double temperature);
+};
+
+#endif
